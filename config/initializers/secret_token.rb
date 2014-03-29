571201cec86e3bf13f75a07e5b463ab93e1421c3
James Dong <jdong8@gmail.com>
2014-03-28 19:22:22 -0700

    finished landing page + precompiled assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Babies::Application.config.secret_key_base = '91d149d3d6e4eba8ef790d6d1b40cd3d8b473cf3049dd4dc076a5ec0e9c224c48522fbb3226aa3f1d8647c1bfd80fdd5291b77a604a28aac6409f8e39be01c91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Babies::Application.config.session_store :cookie_store, key: '_babies_session'
